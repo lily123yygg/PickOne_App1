5d147637cacca275889c67b58c21beb7cdfb0b9c
Aulya Ummaira <2111520007@mahasiswa.atmaluhur.ac.id>
2022-02-18 10:44:15 +0700

    My App

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/ONEPICK/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mhs.ONEPICK;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
